3fc366bdfc9efe9c508d64880ada839a0793de3f
1of1proam <spam1of1proam@gmail.com>
2026-05-12 16:55:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,17 +3,17 @@ import pandas as pd
 
 sheet_id = "1Uui_QzqxqfUVDuL0n84zug2BIyYTHuwAKNbpTNWHgI8"
 
-free_agent_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet=Free Agent"
+free_agent_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet=Free%20Agent"
 
-draft_combine_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet=Draft Combine"
+draft_combine_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet=Draft%20Combine"
 
-draft_combine_1_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet=Draft Combine1"
+draft_combine1_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet=Draft%20Combine1"
 
 free_agent_df = pd.read_csv(free_agent_url)
 
 draft_combine_df = pd.read_csv(draft_combine_url)
 
-draft_combine_1_df = pd.read_csv(draft_combine_1_url)
+draft_combine1_df = pd.read_csv(draft_combine1_url)
 
 st.title("2K League Stats")
 
@@ -24,4 +24,4 @@ st.header("Draft Combine")
 st.dataframe(draft_combine_df)
 
 st.header("Draft Combine1")
-st.dataframe(draft_combine_1_df)
+st.dataframe(draft_combine1_df)
