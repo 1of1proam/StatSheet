9d2cd73d9f3c38072db3817ed828d5eb67e7e099
1of1proam <spam1of1proam@gmail.com>
2026-05-12 17:01:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,18 +10,16 @@ draft_combine_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?
 draft_combine1_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet=DraftCombine1"
 
 free_agent_df = pd.read_csv(free_agent_url)
-
 draft_combine_df = pd.read_csv(draft_combine_url)
-
 draft_combine1_df = pd.read_csv(draft_combine1_url)
 
 st.title("2K League Stats")
 
-st.header("FreeAgent")
+st.header("Free Agent")
 st.dataframe(free_agent_df)
 
-st.header("DraftCombine")
+st.header("Draft Combine")
 st.dataframe(draft_combine_df)
 
-st.header("DraftCombine1")
+st.header("Draft Combine1")
 st.dataframe(draft_combine1_df)
