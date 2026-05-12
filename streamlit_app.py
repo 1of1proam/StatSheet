8d7277b67996b7f786bc2d57ee61e44519fb742a
1of1proam <spam1of1proam@gmail.com>
2026-05-12 17:29:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,25 @@
 import streamlit as st
+import pandas as pd
+from urllib.parse import quote
+
+sheet_id = "1Uui_QzqxqfUVDuL0n84zug2BIyYTHuwAKNbpTNWHgI8"
+
+def load_sheet(tab_name):
+  encoded_tab = quote(tab_name)
+  url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={encoded_tab}"
+  return pd.read_csv(url)
 
 st.title("2K League Stats")
 
-st.write("Select a page from the sidebar.")
+free_agent_df = load_sheet("Free Agent")
+draft_combine_df = load_sheet("Combine Game 1 ")
+draft_combine1_df = load_sheet("Combine Game 2 ")
+
+st.header("Free Agent")
+st.dataframe(free_agent_df)
+
+st.header(Combine Game 1")
+st.dataframe(draft_combine_df)
+
+st.header("Combine Game 2 ")
+st.dataframe(draft_combine1_df)
