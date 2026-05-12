74af9b975ca83bb96eda2d9a19548adc3e5d6b0e
1of1proam <spam1of1proam@gmail.com>
2026-05-12 16:50:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,27 @@
 import streamlit as st
 import pandas as pd
 
-sheet_id = "1Uui_QzqxqfUVDuL0n84zug2BIyYIHuwAKNbpTNWHgI8"
+sheet_id = "1Uui_QzqxqfUVDuL0n84zug2BIyYTHuwAKNbpTNWHgI8"
 
-url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid=0"
+free_agent_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet=Free Agent"
 
-df = pd.read_csv(url)
+draft_combine_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet=Draft Combine"
+
+draft_combine_1_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet=Draft Combine 1"
+
+free_agent_df = pd.read_csv(free_agent_url)
+
+draft_combine_df = pd.read_csv(draft_combine_url)
+
+draft_combine_1_df = pd.read_csv(draft_combine_1_url)
 
 st.title("2K League Stats")
 
-st.dataframe(df)
+st.header("Free Agent")
+st.dataframe(free_agent_df)
+
+st.header("Draft Combine")
+st.dataframe(draft_combine_df)
+
+st.header("Draft Combine 1")
+st.dataframe(draft_combine_1_df)
