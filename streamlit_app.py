f6b35c7ce9eddc83c11c002c91f5507248ac26a6
1of1proam <spam1of1proam@gmail.com>
2026-05-12 16:31:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-sheet_id = "PASTE_YOUR_SHEET_ID_HERE"
+sheet_id = "1Uui_QzqxqfUVDuL0n84zug2BIyYIHuwAKNbpTNWHgI8"
 
 url = f"https://docs.google.com/spreadsheets/d/{1Uui_QzqxqfUVDuL0n84zug2BIyYIHuwAKNbpTNWHgI8}/export?format=csv&gid=0"
 
