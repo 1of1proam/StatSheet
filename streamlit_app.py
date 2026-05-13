63fe0470d4a000d72ebe5b20c333d195964af69a
1of1proam <spam1of1proam@gmail.com>
2026-05-12 20:01:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,11 +16,15 @@ st.title("🏀 1of1 Pro Am")
 free_agent_df = load_sheet("Free Agent")
 combine_game1_df = load_sheet("Combine Game 1 Stats")
 combine_game2_df = load_sheet("Combine Game 2 Stats")
+draft_df = load_sheet("Draft")
+teams_df = load_sheet("Teams")
 
-players_tab, game1_tab, game2_tab = st.tabs([
+players_tab, game1_tab, game2_tab, draft_tab, teams_tab = st.tabs([
   " Free Agent Players",
   " Combine Game 1 Stats",
   " Combine Game 2 Stats"
+  "Draft"
+  "Teams"
 ])
 
 with players_tab:
@@ -34,3 +38,11 @@ with game1_tab:
 with game2_tab:
   st.subheader("Combine Game 2 Stats")
   st.dataframe(combine_game2_df, use_container_width=True)
+
+with draft_tab:
+  st.subheader("Draft")
+  st.dataframe(draft_df, use_container_width=True)
+
+with teams_tab:
+  st.subheader("Teams")
+  st.dataframe(teams_df, use_container_width=True)
